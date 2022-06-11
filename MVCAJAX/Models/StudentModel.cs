67b3bbb3df2f5482e819fa1e2fcc91e1354c851b
Elsin14 <elsin.drafus14@gmail.com>
2022-06-11 14:53:54 -0500

    first commit

---
+++ MVCAJAX/Models/StudentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCAJAX.Models
+{
+    public class StudentModel
+    {
+        public int studentID { get; set; }
+        public string studentName { get; set; }
+        public string studentLastName { get; set; }
+        public int studentCode { get; set; }
+        public string studentAdress { get; set; }
+    }
+}
